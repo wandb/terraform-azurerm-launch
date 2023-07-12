c1183a5069836e116e2c8486e23ddbcf05a7e4aa
Zachary Blasczyk <zachary.blasczyk@wandb.com>
2023-07-11 19:34:31 -0500

    refactor: Format fix

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "helm_release_status" {
-  value = module.launch_helm.helm_release_status
+  value       = module.launch_helm.helm_release_status
   description = "Status of the helm release"
 }
